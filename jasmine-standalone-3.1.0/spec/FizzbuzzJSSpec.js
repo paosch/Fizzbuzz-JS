2749257faac3d00c9cd027c7e356c67225b95c51
Paola Schiaffino <paola_schiaffino@yahoo.co.uk>
2018-06-13 14:11:43 +0100

    Refactored tests: using beforeEach to avoid repetition

--- jasmine-standalone-3.1.0/spec/FizzbuzzJSSpec.js
+++ jasmine-standalone-3.1.0/spec/FizzbuzzJSSpec.js
@@ -1,15 +1,17 @@
 describe('FizzbuzzJS', function(){
   var fizzbuzzJS;
 
+  beforeEach(function(){
+    fizzbuzzJS = new FizzbuzzJS;
+  });
+
   describe('multiples of 3', function(){
     it("knows when a number is divisible by 3", function(){
-      fizzbuzzJS = new FizzbuzzJS;
       expect(fizzbuzzJS.isDivisibleByThree(3)).toBe (true)
     })
   })
   describe('multiples of 5', function(){
     it('knows when a number is divisible by 5', function(){
-      fizzbuzzJS = new FizzbuzzJS;
       expect(fizzbuzzJS.isDivisibleByFive(5)).toBe (true)
     })
   })
