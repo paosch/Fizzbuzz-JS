8096dcd11f8850d8d4bdf989d276ffd90e4f9938
Paola Schiaffino <paola_schiaffino@yahoo.co.uk>
2018-06-13 14:08:23 +0100

    It knows whether a number is a multiple of 5

--- jasmine-standalone-3.1.0/spec/FizzbuzzJSSpec.js
+++ jasmine-standalone-3.1.0/spec/FizzbuzzJSSpec.js
@@ -2,9 +2,15 @@ describe('FizzbuzzJS', function(){
   var fizzbuzzJS;
 
   describe('multiples of 3', function(){
-    it("knows when a number is a multiple of 3", function(){
+    it("knows when a number is divisible by 3", function(){
       fizzbuzzJS = new FizzbuzzJS;
       expect(fizzbuzzJS.isDivisibleByThree(3)).toBe (true)
+    })
+  })
+  describe('multiples of 5', function(){
+    it('knows when a number is divisible by 5', function(){
+      fizzbuzzJS = new FizzbuzzJS;
+      expect(fizzbuzzJS.isDivisibleByFive(5)).toBe (true)
     })
   })
 });
--- jasmine-standalone-3.1.0/src/FizzbuzzJS.js
+++ jasmine-standalone-3.1.0/src/FizzbuzzJS.js
@@ -3,3 +3,7 @@ var FizzbuzzJS = function(){};
 FizzbuzzJS.prototype.isDivisibleByThree = function(number){
   return (number % 3 === 0);
 };
+
+FizzbuzzJS.prototype.isDivisibleByFive = function(number){
+  return (number % 5 === 0);
+};
